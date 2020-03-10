5ab075d66b8c7cd9cad11d1315f7f3946034c036
unknown <fd6hjk@naver.com>
2020-03-10 14:44:18 +0900

    fix notification router

--- src/routers/notification.ts
+++ src/routers/notification.ts
@@ -10,10 +10,25 @@ const notificationRouter = (db : DB) => {
     
     router.post("/register", async (req, res) => {
         const { address, token, birthYear } : NotificationRegister = req.body;
-        if(!address || !token || birthYear) return res.sendStatus(400);
+        if(!address || !token || !birthYear) return res.sendStatus(400);
         const c = await db.getCollection("noti");
-        await c.insert({ address, token, birthYear });
+        await c.insertOne({ address, token, birthYear });
         res.sendStatus(200);
+    });
+
+    router.patch("/register", async (req, res) => {
+        const { address, token, birthYear } : NotificationRegister = req.body;
+        if(!token) return res.sendStatus(400);
+        const c = await db.getCollection("noti");
+        const update : any = {};
+        if(!address) update["address"] = address;
+        if(!birthYear) update["birthYear"] = birthYear;
+        try{
+            await c.updateOne({ token }, { $set : update });
+            res.sendStatus(200);
+        }catch(e){
+            res.sendStatus(500);
+        }
     });
 
     router.delete("/register", async (req, res) => {
